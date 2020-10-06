9fae79809774c49e07d5c7bddeb252c8ca2d288e
wapringle <59368076+wapringle@users.noreply.github.com>
2020-10-06 16:52:03 +0100

    Mock brython timer function

---
+++ browser/timer/__init__.py
@@ -0,0 +1,14 @@
+"""Mock brython timer functions """
+
+def set_timeout(*args,**kwargs):
+    pass
+def clear_timeout(*args,**kwargs):
+    pass
+def set_interval(*args,**kwargs):
+    pass
+def clear_interval(*args,**kwargs):
+    pass
+def request_animation_frame(*args,**kwargs):
+    pass
+def cancel_animation_frame(*args,**kwargs):
+    pass
