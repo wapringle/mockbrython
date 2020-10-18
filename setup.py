1f39c7a260bdad7762652fac339b6b8b648827fb
wapringle <bllprngl@gmail.com>
2020-10-18 12:01:49 +0000

    tidy up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mockbrython", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Bill Pringle",
     author_email="bllprngl@gmail.com",
     description="Mock brython functions for local testing and debugging.",
